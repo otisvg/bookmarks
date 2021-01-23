f523c4e079d61bd2583a252235aa616bfdc450d7
Otis <otisvickersgraver@gmail.com>
2021-01-23 13:05:14 +0000

    edit url to match pg varchar limit

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,7 +1,7 @@
 feature 'viewing bookmarks' do
   scenario 'visiting bookmarks page' do
     visit('/bookmarks')
-    expect(page).to have_content "https://www.edx.org/course/cs50s-introduction-to-computer-science"
+    expect(page).to have_content "https://www.edx.org/course"
     expect(page).to have_content "https://ktt2.com/music"
     expect(page).to have_content "https://www.reddit.com"
   end
